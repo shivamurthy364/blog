f408fdec9f7857adc5e2622c85e7d85a6137333a
shivamurthy367 <shiremath043@gmail.com>
2023-11-23 01:30:06 +0530

    Pushing initial files

---
+++ src/pages/Home.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='mb-20'> 
+        <h1 className='sm:text-4xl text-2xl font-bold my-6 text-gray-900'>Hello welcome to my blog</h1>
+
+        <p className='mx-auto leading-relaxed text-base mb-4'>
+        Dear Shivamurtayya,
+We are excited to invite skilled professionals like you to participate in our upcoming Hackathon, which offers an excellent opportunity to land exciting jobs. Our event has attracted several companies that are looking to fill various roles.
+Multiple companies are on the lookout for different roles.
+Register for the Hackathon most suitable to you.
+Date: 25 th & 26 th November '23 |  Duration: 01 hrs 30 mins |  Mode: Online
+        </p>
+        <p className='mx-auto leading-relaxed text-base mb-4'> ---poroijeeffk;'''''''''''''======------------------------------===========================
+
+        Dear Shivamurtayya,
+We are excited to invite skilled professionals like you to participate in our upcoming Hackathon, which offers an excellent opportunity to land exciting jobs. Our event has attracted several companies that are looking to fill various roles.
+Multiple companies are on the lookout for different roles.
+Register for the Hackathon most suitable to you.
+Date: 25 th & 26 th November '23 |  Duration: 01 hrs 30 mins |  Mode: Online
+====================================
+        </p>
+        
+    </div>
+  )
+}
+
+export default Home
