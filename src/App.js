106770df18d7c8b9526f3a4e6bf62e600c4645f9
shivamurthy367 <shiremath043@gmail.com>
2023-11-24 00:44:27 +0530

    Updated frontend pages

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import Article from "./pages/Article";
 import ArticlesList from "./pages/ArticlesList";
+import NotFound from "./pages/NotFound";
+
+//component
 import Navbar from "./components/Navbar";
 
 function App() {
@@ -15,6 +18,7 @@ function App() {
    <Route path="/about" element={<About />} />
    <Route path="/articles-list" element={<ArticlesList />} />
    <Route path="/article/:name" element={<Article />} />
+   <Route path="*" element={<NotFound />} />
    </Routes>
 
    </div>
--- src/pages/Article.js
+++ src/pages/Article.js
@@ -1,14 +1,19 @@
 import React from "react"
 import { useParams } from "react-router-dom";
 import articleContent from "./Article-Content";
+//page
+import NotFound from "./NotFound";
+
+import Articles from "../components/Articles";
 
 const Article = () => {
   const {name} = useParams();
   const article = articleContent.find((article) => article.name===name);
-  if(!article) return <h1> Article does not exist</h1>
+  if(!article) return <NotFound /> //putting condition for not found error
+  const otherArticles = articleContent.filter(article => article.name !== name)
   return (
-    <div>
-      <h1 className='sm:text-4xl text-2xl font-bold my-6 text-gray-900'> 
+    <>      
+    <h1 className='sm:text-4xl text-2xl font-bold my-6 text-gray-900'> 
       {article.title}
       </h1>
       {article.content.map((paragraph, index)=> (
@@ -16,7 +21,12 @@ const Article = () => {
         {paragraph}
         </p>
       ))}
-    </div>
+
+      <h1 className="sm:text-2xl text-xl font-bold my-4 text-gray-900"> Other Articles</h1>
+      <div className="flex flex-wrap -m-4">
+        <Articles articles={otherArticles}/>
+      </div>
+    </>
   )
 }
 
